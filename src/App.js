c567574be6f1a211711d0faf006f8a840a9da7a2
leonore14 <63911892+leonore14@users.noreply.github.com>
2020-09-04 23:44:59 +0200

    adding routing and api

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/NavBar/Navbar';
-import Home from './components/Home/Home';
 import SelectorTool from './components/SelectorTool/SelectorTool';
-import Banner from './components/Banner';
+import Home from './components/Home/Home';
 import Footer from './components/Footer/Footer'
-
 
 
 
@@ -16,8 +14,12 @@ function App() {
 
     <div className="appDiv">
       <Navbar />
-      <Banner />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/SelectorTool' component={SelectorTool} />
+      </Switch>
       <Footer />
+
     </div>
 
   );
