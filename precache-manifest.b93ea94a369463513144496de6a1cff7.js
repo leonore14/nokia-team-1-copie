fa6ca867a0d676e2eb39f40c2d91ca15187d6b17
leonore14 <63911892+leonore14@users.noreply.github.com>
2020-09-12 13:55:59 +0200

    Updates

---
+++ precache-manifest.b93ea94a369463513144496de6a1cff7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df89757621df949b87c1a10393422f7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db120cdbf513d0afa112",
+    "url": "/static/css/main.a24d82f2.chunk.css"
+  },
+  {
+    "revision": "1feb5e4851b197ac1d0e",
+    "url": "/static/js/2.20d7556d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.20d7556d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db120cdbf513d0afa112",
+    "url": "/static/js/main.02ab51c0.chunk.js"
+  },
+  {
+    "revision": "a5c4fa068015e9b16222",
+    "url": "/static/js/runtime-main.98744d38.js"
+  },
+  {
+    "revision": "52a451b4ebce322d91005829ab035998",
+    "url": "/static/media/1.4efaa061.jpg"
+  },
+  {
+    "revision": "830ff5f6d9facf8561984b8e4071258b",
+    "url": "/static/media/2.0b4d8863.jpg"
+  },
+  {
+    "revision": "5c9b115b75805e97e6f18a487d4d887a",
+    "url": "/static/media/3.643692e2.jpg"
+  },
+  {
+    "revision": "ca4a835567b564afc9db4bb05275bcbb",
+    "url": "/static/media/4.d5bfaed6.jpg"
+  },
+  {
+    "revision": "54f5f036ceff481e3e0a914753ebe364",
+    "url": "/static/media/5.82c21953.jpg"
+  },
+  {
+    "revision": "29db2c31b5b7962dc74ca26f74444786",
+    "url": "/static/media/HELP-logo.d6197f8f.gif"
+  },
+  {
+    "revision": "e1f1cdc9c8afc51e38aef9114893209f",
+    "url": "/static/media/NokiaPureHeadline_Bd.e1f1cdc9.ttf"
+  },
+  {
+    "revision": "2cb2263e964fdc30c500094435cc3b30",
+    "url": "/static/media/NokiaPureHeadline_Lt.2cb2263e.ttf"
+  },
+  {
+    "revision": "d10dc635a994f5d8ce20b91d4273705d",
+    "url": "/static/media/NokiaPureHeadline_Rg.d10dc635.ttf"
+  },
+  {
+    "revision": "2bf9c690596483e25886e84275c68213",
+    "url": "/static/media/NokiaPureHeadline_ULt.2bf9c690.ttf"
+  },
+  {
+    "revision": "dfd8757f6aab67e6125c3575dfb79577",
+    "url": "/static/media/NokiaPureHeadline_XBd..dfd8757f.ttf"
+  },
+  {
+    "revision": "d3eadc8d2baa41ab6ffebac26a3cf6b3",
+    "url": "/static/media/NokiaPureTWeb_Bd.d3eadc8d.ttf"
+  },
+  {
+    "revision": "abb1b26db4e48c3b62940e45b06d87f9",
+    "url": "/static/media/NokiaPureTWeb_Lt.abb1b26d.ttf"
+  },
+  {
+    "revision": "536160e84aba3fb83504066dd068bed3",
+    "url": "/static/media/NokiaPureTWeb_Rg.536160e8.ttf"
+  },
+  {
+    "revision": "762af741c73c64ebdc4992e0d5208077",
+    "url": "/static/media/PhonesForEveryoneBanner.d2eae700.png"
+  },
+  {
+    "revision": "ffdf93c352b5888d1730c8dc9c8d4008",
+    "url": "/static/media/TopBanner.cc0ca095.jpg"
+  },
+  {
+    "revision": "7318768047de252e999fcf4c3a057f2b",
+    "url": "/static/media/YourMatch.465d4df4.jpg"
+  }
+]);
